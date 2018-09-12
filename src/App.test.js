9704ccb178aece4d13fc5ae2fb855c8c1bdb74fe
Ross Cooper <rossjamescooper@gmail.com>
2018-09-12 05:08:45 -0700

    A few notes for future work

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; //decided to leave the App outside of the components folder. Makes sense to me. RC
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/store.js
+++ src/store.js
@@ -4,3 +4,7 @@ import reducer from './reducers/reducer'
 const store = createStore(reducer)
 
 export default store
+
+
+
+///use combineReducers if we decide to use more. RC
